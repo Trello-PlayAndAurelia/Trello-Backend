2608022a7a8e5ab16c56e118dad16ce0a076a9eb
AdamPiech <adampiech7@op.pl>
2016-10-12 21:17:56 +0200

    Created skeleton for API.

---
+++ app/controllers/data/UserController.java
@@ -0,0 +1,26 @@
+package controllers.data;
+
+import play.mvc.Result;
+
+/**
+ * Created by Adam Piech on 2016-10-12.
+ */
+public class UserController {
+
+    public Result get(Integer id) {
+        return null;
+    }
+
+    public Result create() {
+        return null;
+    }
+
+    public Result update() {
+        return null;
+    }
+
+    public Result delete(Integer id) {
+        return null;
+    }
+
+}
